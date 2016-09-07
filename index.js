ea62d252eadf9fc615ba07456956fd712ef00209
Jamen Marzonie <jamenmarz@gmail.com>
2016-09-06 22:15:35 -0600

    Clean code.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var Token = function Token() {};
+var Token = function Token(x) { this.name = x; };
 var slice = Array.prototype.slice;
 
 lazy.Token = Token;
 module.exports = lazy;
 
 // Tokens
-var $select = lazy.select = new Token();
-var $get = lazy.get = new Token();
+var $select = lazy.select = new Token('select');
+var $add = lazy.add = new Token('add');
 
 // Turn a value lazy.
 function lazy(value) {
@@ -22,7 +22,7 @@ function lazy(value) {
   function queue(key) {
     // Handling a function queue
     if (typeof key === 'function') {
-      stack.unshift([$get, [key], arguments[1]]);
+      stack.unshift([key, null, arguments[1]]);
     }
 
     // Handling an array/string queue.
@@ -30,11 +30,16 @@ function lazy(value) {
       var ignore = false;
       var operation = key instanceof Array ? key : slice.call(arguments);
       var method = operation[0];
-      if (method[0] === '~') {
-        method = method.slice(1);
-        ignore = true;
+
+      // Handle method operators.
+      switch (method[0]) {
+        case '~': {
+          method = method.slice(1);
+          ignore = true;
+        }
       }
 
+      // Push results on stack.
       stack.unshift([method, slice.call(operation, 1), ignore]);
     }
 
@@ -47,22 +52,24 @@ function lazy(value) {
       var operation = stack[i];
       var method = operation[0];
       var args = operation[1];
-      var ignore = operation[2];
+      var out = null;
 
-      // Handle "select" token
+      // Handle tokens.
       if (method === $select) value = value[args[0]];
+      else if (method === $add) value += args[0];
 
-      // Handle "run" token.
-      else if (method === $get) {
-        var out = args[0](value);
-        if (!ignore && out || ignore === false) value = out;
+      // Handle function supplied.
+      else if (typeof method === 'function') {
+        out = method(value);
       }
 
       // Handle using native methods.
       else {
-        var out = value[method].apply(value, args);
-        if (!ignore && out || ignore === false) value = out;
+        out = value[method].apply(value, args);
       }
+
+      var ignore = operation[2];
+      if (!ignore && out) value = out;
     }
 
     // Return resulting value.
